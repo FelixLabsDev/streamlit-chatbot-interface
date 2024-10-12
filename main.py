7df38eee79c92c220f4315b15d17f394fc8f14c2
EdEn-D <felixlabsdev@gmail.com>
2024-10-12 18:27:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 base_agent_path = r""
 graph = Graph(base_path=base_agent_path, clean=True)
 
+
 # Mock AI response generation function (replace with actual AI model logic)
 def generate_ai_response(user_input):
     response = graph.invoke_graph(user_input, "052", "eden")
@@ -18,27 +19,30 @@ def generate_ai_response(user_input):
 
 
 # Endpoint to receive user input and return AI-generated response
-@app.route('/input', methods=['POST'])
+@app.route("/input", methods=["POST"])
 def receive_input():
-    user_input = request.json.get('user_input')
-    
+    user_input = request.json.get("user_input")
+
     if user_input:
         # Generate AI response based on the user input
-        ai_response = generate_ai_response(user_input) 
+        ai_response = generate_ai_response(user_input)
 
         # Return the AI response back to Streamlit
         return jsonify({"status": "success", "ai_response": ai_response}), 200
-    
+
     return jsonify({"status": "error", "message": "No input received"}), 400
 
+
 # Endpoint to delete chat history
-@app.route('/delete_history', methods=['POST'])
+@app.route("/delete_history", methods=["POST"])
 def delete_history():
     try:
         # Delete chat history
         graph.delete_chat_history()
         # Return the AI response back to Streamlit
-        return jsonify({"status": "success", "graph_response": "graph history deleted"}), 200
+        return jsonify(
+            {"status": "success", "graph_response": "graph history deleted"}
+        ), 200
     except Exception as e:
         print(f"Error occurred while deleting chat history: {e}")
         return jsonify({"status": "error", "Error": str(e)}), 400
@@ -46,16 +50,20 @@ def delete_history():
 
 # Function to run the Flask app
 def run_flask():
-    app.run(port=5000, debug=True,  use_reloader=False)
+    app.run(port=5000, debug=True, use_reloader=False)
+
 
 def run_view():
     # Start Flask app in a separate thread
     flask_thread = threading.Thread(target=run_flask)
-    flask_thread.daemon = True  # This makes the Flask thread exit when the main program exits
+    flask_thread.daemon = (
+        True  # This makes the Flask thread exit when the main program exits
+    )
     flask_thread.start()
 
     # Start Streamlit app
-    run()
+    run("BOT NAME")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run_view()
