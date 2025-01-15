2f19e01b7400a9bdcbab9ce4184c40616b43321f
EdEn-D <felixlabsdev@gmail.com>
2025-01-15 13:35:45 +0200

    fixed controller

--- controller/controller.py
+++ controller/controller.py
@@ -15,19 +15,19 @@ class Orchestrator():
     # Callback function for the view which will typically call the model to generate a response
     # Supported message types: text, image, audio
     def view_callback(self, data_dict):
+        print("Hello, you have reached the callback, it is printed below:")
         if data_dict["type"] == "unsupported":
             self.view.send_message(data_dict["chat_id"], "Oops, I don't understand that type of message!")
         elif data_dict["type"] == "delete_history":
             self.model.delete_chat_history()
             return
-        print("Hello, you have reached the callback, it is printed below:")
 
         if data_dict["type"] == "text":
             prompt = data_dict["text"]
             thread_id = int(data_dict.get("chat_id", 69))
             user_id = data_dict.get("name", "felix")
 
-        model_response = self.model.invoke_graph(prompt, thread_id, user_id)
+        model_response = self.model.invoke_graph(prompt, thread_id)
         return self.view.send_message(data_dict["chat_id"], model_response)
         
     def run(self, **kwargs):
