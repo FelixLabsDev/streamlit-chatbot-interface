1a7c9edf5bf88578527f65cb0722063c9fd8607e
EdEn-D <felixlabsdev@gmail.com>
2025-03-19 19:46:28 +0200

    stiull not woring

--- src/streamlit_view/view.py
+++ src/streamlit_view/view.py
@@ -2,22 +2,30 @@ import os
 import subprocess
 import requests
 import logging
+import asyncio
 from .view_configurations import define_endpoints
+from .view_abc import BaseView, RedisEnabledMixin
+
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger("view")
 
-class View:
+class View(RedisEnabledMixin, BaseView):
+    
     def __init__(self, app, view_callback, title="Streamlit Chatbot Interface"):
         logging.info("Initializing View class")
-        define_endpoints(app, view_callback)
+        define_endpoints(app, view_callback, self.get_response_callback)
         # self.run(title=title)
-
-    def send_message(self, chat_id, message):
-        return message
+        
+    def set_redis_client(self, redis_client):
+        super().set_redis_client(redis_client)
+        
+    async def send_message(self, chat_id, message):
+        await self.redis.store_ai_response(chat_id, message)
 
     def run_streamlit(self, title):
+        logger.info("Running Streamlit app")
         try:
             filename = os.path.join(os.path.dirname(__file__), "streamlit_chat_ui.py")
             # command = ["streamlit", "run", filename, "--", "--clean", "--title", title]
@@ -30,24 +38,47 @@ class View:
             subprocess.run(command, check=True, env=env)
         except subprocess.CalledProcessError as e:
             logging.error(f"Error occurred while running command: {e}")
+            
+    async def get_response_callback(self, thread_id: str) -> str:
+        """Get AI response for a specific chat"""
+        if self.redis and self.redis.client:
+            return await self.redis.get_first_ai_response(thread_id)
+        return None
 
-    def run(self, title="Streamlit Chatbot Interface"):
+    async def run(self, title="Streamlit Chatbot Interface"):
+        # Offload the blocking call to a thread
+        await asyncio.to_thread(self.run_streamlit, title)
+        
+    def run_sync(self, title="Streamlit Chatbot Interface"):
+        # Optionally, you can call the sync version from your main code:
         self.run_streamlit(title)
 
 
-def send_input(user_input):
+def send_input(user_input, thread_id="_"):
     logger.info("Inside send_input")
     logger.info(f"User input: {user_input}")
     try:
         # Sending the user input to FastAPI and receiving AI response
         response = requests.post(
-            "http://localhost:5051/input", json={"user_input": user_input}
+            "http://localhost:5051/input", json={"user_input": user_input, "thread_id": thread_id}
         )
-        logger.info(f"Response: {response}")
+        logger.info(f"Response View: {response.content}")
         if response.status_code == 200:
-            return response.json().get("ai_response", "No AI response")
+            return response.content
         else:
             return "Error: Failed to get AI response"
+    except requests.exceptions.RequestException as e:
+        return f"Error: {str(e)}"
+
+def get_response(thread_id):
+    logger.info(f"Inside get_response for thread_id: {thread_id}")
+    try:
+        # Sending the user input to FastAPI and receiving AI response
+        response = requests.get(
+            f"http://localhost:5051/get_response?thread_id={thread_id}"
+        )
+        logger.info(f"Response OLD: {response}")
+        return response
     except requests.exceptions.RequestException as e:
         return f"Error: {str(e)}"
 
--- src/streamlit_view/view_configurations.py
+++ src/streamlit_view/view_configurations.py
@@ -5,36 +5,48 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-def define_endpoints(app, view_callback):
+def define_endpoints(app, view_callback, get_response_callback):
     # Endpoint to receive user input and return AI-generated response
     @app.post("/input")
     async def receive_input(request: Request):
         logger.info("inside receive_input")
         data = await request.json()
         user_input = data.get("user_input")
+        thread_id = data.get("thread_id")
 
         if user_input:
-            # Generate AI response based on the user input
+            # Store user input in Redis
             data_dict = {
+                # "user_id": user_id,
                 "type": "text",
-                "chat_id": "69420",
+                "thread_id": thread_id,
                 "text": user_input
             }
-            ai_response = view_callback(data_dict)
-
-            # Return the AI response back to Streamlit
-            return JSONResponse({"status": "success", "ai_response": ai_response}, status_code=200)
+            await view_callback(data_dict)
+            return JSONResponse({"status": "success"}, status_code=200)
 
         raise HTTPException(status_code=400, detail="No input received")
+        
+    # Endpoint to fetch AI response from Redis
+    @app.get("/get_response")
+    async def get_response(thread_id: str):
+        # Fetch AI response from Redis
+        try:
+            ai_response = await get_response_callback(thread_id)
+            if ai_response:
+                return JSONResponse({"status": "success", "ai_response": ai_response}, status_code=200)
+            return JSONResponse({"status": "pending"}, status_code=200)
+        except Exception as e:
+            logger.error(f"Error fetching AI response: {e}")
+            raise HTTPException(status_code=500, detail=str(e))
 
-    # Endpoint to delete chat history
-    @app.post("/delete_history")
+    # Endpoint to delete all chat history
+    @app.post("/delete_all_history")
     async def delete_history():
         try:
-            # Delete chat history
+            # Delete all chat history
             data_dict = {
                 "type": "delete_history", 
-                "chat_id": "69420"
             }
             view_callback(data_dict)
             # Return the AI response back to Streamlit
@@ -42,3 +54,19 @@ def define_endpoints(app, view_callback):
         except Exception as e:
             print(f"Error occurred while deleting chat history: {e}")
             raise HTTPException(status_code=400, detail=str(e))
+        
+    # Endpoint to delete chat history by chat ID
+    @app.post("/delete_chat")
+    async def delete_chat(request: Request):
+        data = await request.json()
+        chat_id = data.get("chat_id")
+        if chat_id:
+            # Delete chat history by chat ID
+            data_dict = {
+                "type": "delete_chat", 
+                "chat_id": chat_id
+            }
+            view_callback(data_dict)
+            # Return the AI response back to Streamlit
+            return JSONResponse({"status": "success", "graph_response": "chat history deleted"}, status_code=200)
+        raise HTTPException(status_code=400, detail="No chat ID received")
